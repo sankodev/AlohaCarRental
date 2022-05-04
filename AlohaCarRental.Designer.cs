e5dabf56b67c4769d70ef5eab95a57d745813b3e
sankodev <104733084+sankodev@users.noreply.github.com>
2022-05-03 17:44:11 -0700

    Add files via upload

---
+++ AlohaCarRental.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Project6_KS_AlohaCarRental
+{
+    partial class AlohaCarRental
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.daysTextBox = new System.Windows.Forms.TextBox();
+            this.mileageTextBox = new System.Windows.Forms.TextBox();
+            this.carTypeTextBox = new System.Windows.Forms.TextBox();
+            this.customerTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.totalCostLabel = new System.Windows.Forms.Label();
+            this.insuranceCostLabel = new System.Windows.Forms.Label();
+            this.insurancePlanLabel = new System.Windows.Forms.Label();
+            this.mileageChargeLabel = new System.Windows.Forms.Label();
+            this.dailyChargeLabel = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kaleb Sandin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ITS 128";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Project 6";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.daysTextBox);
+            this.groupBox1.Controls.Add(this.mileageTextBox);
+            this.groupBox1.Controls.Add(this.carTypeTextBox);
+            this.groupBox1.Controls.Add(this.customerTextBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(139, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 203);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rental Information:";
+            // 
+            // daysTextBox
+            // 
+            this.daysTextBox.Location = new System.Drawing.Point(183, 159);
+            this.daysTextBox.Name = "daysTextBox";
+            this.daysTextBox.Size = new System.Drawing.Size(100, 20);
+            this.daysTextBox.TabIndex = 3;
+            // 
+            // mileageTextBox
+            // 
+            this.mileageTextBox.Location = new System.Drawing.Point(183, 119);
+            this.mileageTextBox.Name = "mileageTextBox";
+            this.mileageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.mileageTextBox.TabIndex = 2;
+            // 
+            // carTypeTextBox
+            // 
+            this.carTypeTextBox.Location = new System.Drawing.Point(183, 75);
+            this.carTypeTextBox.Name = "carTypeTextBox";
+            this.carTypeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.carTypeTextBox.TabIndex = 1;
+            // 
+            // customerTextBox
+            // 
+            this.customerTextBox.Location = new System.Drawing.Point(183, 30);
+            this.customerTextBox.Name = "customerTextBox";
+            this.customerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.customerTextBox.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Enter Number of Days:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Enter Mileage:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Enter Car Type:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Enter Customer Number:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.totalCostLabel);
+            this.groupBox2.Controls.Add(this.insuranceCostLabel);
+            this.groupBox2.Controls.Add(this.insurancePlanLabel);
+            this.groupBox2.Controls.Add(this.mileageChargeLabel);
+            this.groupBox2.Controls.Add(this.dailyChargeLabel);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(139, 248);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(306, 249);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Rental Charges:";
+            // 
+            // totalCostLabel
+            // 
+            this.totalCostLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalCostLabel.Location = new System.Drawing.Point(183, 203);
+            this.totalCostLabel.Name = "totalCostLabel";
+            this.totalCostLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalCostLabel.TabIndex = 4;
+            // 
+            // insuranceCostLabel
+            // 
+            this.insuranceCostLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.insuranceCostLabel.Location = new System.Drawing.Point(183, 161);
+            this.insuranceCostLabel.Name = "insuranceCostLabel";
+            this.insuranceCostLabel.Size = new System.Drawing.Size(100, 23);
+            this.insuranceCostLabel.TabIndex = 3;
+            // 
+            // insurancePlanLabel
+            // 
+            this.insurancePlanLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.insurancePlanLabel.Location = new System.Drawing.Point(183, 124);
+            this.insurancePlanLabel.Name = "insurancePlanLabel";
+            this.insurancePlanLabel.Size = new System.Drawing.Size(100, 23);
+            this.insurancePlanLabel.TabIndex = 2;
+            // 
+            // mileageChargeLabel
+            // 
+            this.mileageChargeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.mileageChargeLabel.Location = new System.Drawing.Point(183, 83);
+            this.mileageChargeLabel.Name = "mileageChargeLabel";
+            this.mileageChargeLabel.Size = new System.Drawing.Size(100, 23);
+            this.mileageChargeLabel.TabIndex = 1;
+            // 
+            // dailyChargeLabel
+            // 
+            this.dailyChargeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dailyChargeLabel.Location = new System.Drawing.Point(183, 45);
+            this.dailyChargeLabel.Name = "dailyChargeLabel";
+            this.dailyChargeLabel.Size = new System.Drawing.Size(100, 23);
+            this.dailyChargeLabel.TabIndex = 0;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(26, 204);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Total Cost:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(26, 162);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(81, 13);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "Insurance Cost:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(26, 125);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(126, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Insurance Plan Selected:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 84);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(111, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Total Mileage Charge:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(26, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Total Daily Charge:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(85, 517);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 1;
+            this.calculateButton.Text = "&Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(227, 517);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "C&lear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(381, 517);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // AlohaCarRental
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 564);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AlohaCarRental";
+            this.Text = "Aloha Car Rental";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox daysTextBox;
+        private System.Windows.Forms.TextBox mileageTextBox;
+        private System.Windows.Forms.TextBox carTypeTextBox;
+        private System.Windows.Forms.TextBox customerTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label totalCostLabel;
+        private System.Windows.Forms.Label insuranceCostLabel;
+        private System.Windows.Forms.Label insurancePlanLabel;
+        private System.Windows.Forms.Label mileageChargeLabel;
+        private System.Windows.Forms.Label dailyChargeLabel;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ AlohaCarRental.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project6_KS_AlohaCarRental
+{
+    public partial class AlohaCarRental : Form
+    {
+        private decimal calculateDailyCharge(int days, int carType, decimal dailyCharge, decimal chargePerDay)
+        {
+            
+            if (carType == 1)
+            {
+                chargePerDay = 20.00m;
+            }
+            else if (carType == 2)
+            {
+                chargePerDay = 25.00m;
+            }
+            else
+            {
+                chargePerDay = 32.00m;
+            }
+            dailyCharge = days * chargePerDay;
+
+            return dailyCharge;
+        }
+
+        private decimal calculateMileageCharge(int days, int carType, decimal mileageCharge, decimal chargePerMile)
+        {
+            if (carType == 1)
+            {
+                chargePerMile = 0.25m;
+            }
+            else if (carType == 2)
+            {
+                chargePerMile = 0.35m;
+            }
+            else
+            {
+                chargePerMile = 0.40m;
+            }
+            mileageCharge = days * chargePerMile;
+            return mileageCharge;
+        }
+        public AlohaCarRental()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            customerTextBox.Clear();
+            carTypeTextBox.Clear();
+            mileageTextBox.Clear();
+            daysTextBox.Clear();
+            dailyChargeLabel.Text = "";
+            mileageChargeLabel.Text = "";
+            insurancePlanLabel.Text = "";
+            insuranceCostLabel.Text = "";
+            totalCostLabel.Text = "";
+            customerTextBox.Focus();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //prep
+            string customer;
+            int carType, days;
+            double mileage;
+
+            //vali
+            if (!double.TryParse(mileageTextBox.Text, out mileage)|| mileage < 0)
+            {
+                MessageBox.Show("Enter in the mileage on the rental.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);  
+            }
+            if (!int.TryParse(daysTextBox.Text, out days)||days <0)
+            {
+               MessageBox.Show("Enter in the days on the rental.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            if (!int.TryParse(carTypeTextBox.Text, out carType)|| carType <1 || carType > 3) 
+            {
+               MessageBox.Show("Enter in the insurance plan type on the rental.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            //useless
+            customer = customerTextBox.Text;
+
+            
+        }
+    }
+}
